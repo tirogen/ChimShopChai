4626e8fe53c662fe3509d7c9a81d24a6abb7ca1c
tirogen <the-px@hotmail.com>
2019-09-30 00:45:57 +0700

    handle error

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+async function goto(){
     const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    let data = "";
-    do{
-        await page.goto('https://regist.xn--b3caa1e2a7e2b0h2be.com/Register/');
-        data = await page.evaluate(() => {return document.documentElement.innerHTML});
+    try{
+        const page = await browser.newPage();
+        let data = "";
+        do{
+            await page.goto('https://regist.xn--b3caa1e2a7e2b0h2be.com/Register/');
+            data = await page.evaluate(() => {return document.documentElement.innerHTML});
+        }while(data.includes(`You are currently waiting for the registration queue`));
+        return true;
+    }catch(e){
+        await browser.close();
+        return false;
     }
-    while(data.includes(`You are currently waiting for the registration queue`));
+};
+
+(async () => {
+    let result = false;
+    do{
+        result = await goto();
+    }while(!result);
 })();
